02a0b4d8e787897289427f8eb643a953a3c66555
calixtollucas <lucaslucenacalixto1@gmail.com>
2026-05-10 14:53:24 -0300

    refactor jobs table

--- src/main/java/com/lucasodev/ConsultoriaDeImagem/job/service/JobService.java
+++ src/main/java/com/lucasodev/ConsultoriaDeImagem/job/service/JobService.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class JobService {
 
-    private JobRepository jobRepository;
+    //private JobRepository jobRepository;
     //private AnalysisService analysisService;
 
     public JobResultDTO processJob(AnalysisRequestDTO analysisRequest) {
